32833b749a99b28ff0cc5f0984ef76606e49c1dd
cneagoe <ng.ciprian@gmail.com>
2023-07-10 21:01:37 +0300

    Create bash_crash_course.sh

---
+++ bash_crash_course.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+# NAVIGATION
+# ls : Display the contents of the directory. 
+# If you call the command without parameters, it shows you
+# the contents of the current directory.
+ls 
+# pwd : Display the path to the current directory.
+# When you add the -W parameter, the command displays the
+# path in the Windows directory structure.
+pwd
+# cd : Go to the directory at the specified 
+# relative or absolute path. 
+cd tmp
+cd .. 
+# mount : Mount the disk to the root file system. If you call the
+# command without parameters, it shows a list of all
+# mounted disks.
+mount
+# find : Find a file or directory. The first parameter find . -name vim
+# specifies the directory to start searching. 
+touch /tmp/somevimfile
+find /tmp -name *vim*
+# touch : create a new file
+touch test.txt
+# echo : print something
+# > : redirect output of a command 
+echo "some PATH" > test.txt 
+# grep : Find a file by its contents. 
+grep "PATH" *
+grep -Rn "PATH" .
+grep "PATH" * .*
+# find : find a file or directory by its name 
+find . -name "*.txt"
