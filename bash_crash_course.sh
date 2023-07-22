013848d3df9a5e9dc35b1003346b891590ce1b53
cneagoe <ng.ciprian@gmail.com>
2023-07-22 18:14:10 +0300

    Update bash_crash_course.sh

--- bash_crash_course.sh
+++ bash_crash_course.sh
@@ -12,41 +12,50 @@ history
 echo $SHELL
 # check the PATH variable
 echo $PATH
+
 # ls : Display the contents of the directory. 
 # If you call the command without parameters, it shows you
 # the contents of the current directory.
 ls 
+
 # pwd : Display the path to the current directory.
 # When you add the -W parameter, the command displays the
 # path in the Windows directory structure.
 pwd
+
 # cd : Go to the directory at the specified 
 # relative or absolute path. 
 cd /tmp
 cd .. 
 cd ~
+
 # create a new empty file at a location
 touch /tmp/somevimfile
 # find : Find a file or directory. 
-# The first parameter find . -name vim specifies the directory to start searching. 
+# see glob link
 find /tmp -name *vim*
 # touch : create a new file
 touch test.txt
 touch some.yml
 touch some_git.yml
-# echo : print something
-# > and >> : redirect output of a command 
-echo "some PATH" > test.txt 
-echo "some other PATH" >> test.txt 
+
 # grep : Search in the contents of a file or directory. 
 grep "PATH" *
 grep -Rn "PATH" .
 grep "PATH" * .*
+
 # find : Search for a file or directory with a certain name or type 
 find . -name "*.txt"
+
 # pipe : send the output of one command to another
 find . -name "*.yml" | grep "git" > tst.txt
 history | grep ls
+
+# echo : print something
+# > and >> : redirect output of a command 
+echo "some PATH" > test.txt 
+echo "some other PATH" >> test.txt 
+
 # file permissions
 touch script.sh
 echo '#!/bin/bash' > script.sh
@@ -62,6 +71,7 @@ bash script.sh
 chmod +x script.sh
 # try to execute the script again
 ./script.sh
+
 # remove the script
 rm script.sh
 
@@ -69,14 +79,38 @@ rm script.sh
 export IRONMAN=Tony
 echo $IRONMAN
 
-# create a variable in a script
+# create a local variable
 NAME=value
 
 # file paths
 # absolute path example
-/home/codio/workspace
+ls /home/codio/workspace
 # relative path example
-./workspace
+ls ./workspace
 
+# sudo (super user do)
+# run current command as root user
+# can be put in front of any command
 
+# package manager
+# ubuntu apt
 
+# update package lists
+apt-get update
+
+# install software
+apt-get install apache2
+
+# service management
+
+# set the service to run at boot time
+systemctl enable apache2
+
+# start a service
+sudo systemctl start apache2
+
+# check service status
+sudo systemctl status apache2
+
+# generate an ssh key
+ssh-keygen -t rsa
