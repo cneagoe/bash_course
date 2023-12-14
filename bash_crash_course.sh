e96b3bd251c06549ac99598d2641b89206dfee53
cneagoe <ng.ciprian@gmail.com>
2023-12-14 16:55:13 +0200

    Update bash_crash_course.sh

--- bash_crash_course.sh
+++ bash_crash_course.sh
@@ -1,17 +1,27 @@
 #!/bin/bash
 # BASIC COMMANDS 
 
-# accessing documentation
-man ls
+# alias 
+# usualy a shortened form of a command
+# see ll example in ~/.bashrc
 
-# view history of commands run so far
-history
+# see if command is alias, internal or external
+type pwd
+type man
+type ll
+
+# internal command is part of the shell
+# external command is found on disk through the PATH variable
 
 # environment variables
 # find out what shell you're using
 echo $SHELL
 # check the PATH variable
 echo $PATH
+# $PATH variable can be set for specific users
+
+# shows path to bin if the path is already present in $PATH
+which man
 
 # ls : Display the contents of the directory. 
 # If you call the command without parameters, it shows you
@@ -28,6 +38,246 @@ pwd
 cd /tmp
 cd .. 
 cd ~
+
+# exercises
+# 1. Type time ls. This executes the ls command where the Bash internal time
+# shows information about the time it took to complete this command.
+# 2. Type which time. This shows the filename /usr/bin/time that was found in the
+# $PATH variable.
+# 3. Type time, which shows that time is a shell keyword.
+# 4. Type echo $PATH to show the contents of the $PATH variable. You can
+# see that /usr/bin is included in the list, but because there also is an internal
+# command time, the time command from the path will not be executed unless
+# you tell the shell specifically to do so—the command in step 2 has executed the
+# internal command for you because of command precedence.
+# 5. Type /usr/bin/time ls to run the /usr/bin/time command when executing
+# ls. You’ll notice that the output differs completely. Ignore the meaning of the
+# output; we get back to that later. What matters for now is that you realize that
+# these are really two different commands.
+
+Standard Input, Output, and Error 
+Name    File Descriptor Number
+STDIN   0
+STDOUT  1
+STDERR  2
+
+Common Bash Redirectors
+Redirector        Explanation
+> (same as 1>)    Redirects STDOUT. If redirection is to a file, the current contents of that file are overwritten.
+>> (same as 1>>)  Redirects STDOUT in append mode. If output is written to a file, the output is appended to that file.
+2>                Redirects STDERR.
+2>&1              Redirects STDERR to the same destination as STDOUT. Notice that this has to be used in combination with normal output redirection,as in ls whuhiu > errout 2>&1.
+< (same as 0<)    Redirects STDIN.
+
+# pipe : send the output of one command to another
+history | grep ls
+
+# exercises
+# 1. Type ls. You’ll see the ls command output onscreen.
+# 2. Type ls > /dev/null. This redirects STDOUT to the null device, with the result
+# that you will not see it.
+# 3. Type ls ilwehgi > /dev/null. This command shows a “no such file or directory”
+# message onscreen. You see the message because it is not STDOUT, but rather an
+# error message that is written to STDERR.
+# 4. Type ls ilwehgi 2> /dev/null. Now you will no longer see the error message.
+# 5. Type ls ilwehgi /etc 2> /dev/null. This shows the contents of the /etc folder
+# while hiding the error message.
+# 6. Type ls ilwehgi /etc 2> /dev/null > output. In this command, you still write
+# the error message to /dev/null while sending STDOUT to a file with the name
+# output that will be created in your home directory.
+# 7. Type cat output to show the contents of this file.
+# 8. Type echo hello > output. This overwrites the contents of the output file. Verify
+# this by using cat output again.
+# 9. Type ls >> output. This appends the result of the ls command to the output file.
+# Type cat output to verify.
+# 10. Type ls -R /. This shows a long list of files and folders scrolling over your
+# computer monitor. (You might want to press Ctrl-C to stop [or wait some time]).
+# 11. Type ls -R /. | less. This shows the same result, but in the less pager, where you
+# can scroll up and down using the arrow keys on your keyboard.
+# 12. Type q to close less. This will also end the ls program.
+# 13. Type ls > /dev/tty1. This gives an error message because you are executing the
+# command as an ordinary user, and ordinary users cannot address device files
+# directly (unless you were logged in to tty1). Only the user root has permission to
+# write to device files directly.
+
+# view history of commands run so far
+history
+# Type !number to execute a command with a specific number from history.
+# Type !sometext to execute the last command that starts with sometext. 
+# Use history -d number to delete a specific command from history. 
+# type history -c to clear the current history.
+# if you want to remove both the current history and the contents of the .bash_history file,
+# then type history -w immediately after running the history -c command.
+
+# exercises
+# 1. Type history to get an overview of commands that you have previously used.
+# 2. Type some commands, such as the following:
+# ls
+# pwd
+# cat /etc/hosts
+# ls –l
+# The goal is to fill the history a bit.
+# 3. Open a second terminal on your server. To do so, click Activities in the upper-
+# left corner, and in the Search bar, type term. Next, click the terminal window to
+# start it.
+# 4. Type history from this second terminal window. Notice that you do not see
+# the commands that you just typed in the other terminal. The reason is that the
+# history file has not been updated yet.
+# 5. From the first terminal session, press Ctrl-r. From the prompt that opens now,
+# type ls. You’ll see the last ls command you used. Press Ctrl-r again. You’ll now see
+# that you are looking backward and that the previous ls command is highlighted.
+# Press Enter to execute it.
+# 6. Type history | grep cat. The grep command searches the history output for any
+# commands that contain the text cat. Note the command number of one of the cat
+# commands you have previously used.
+# 7. Type !nn, where nn is replaced by the number you noted in step 7. You’ll see that
+# the last cat command is repeated.
+# 8. Close this terminal by typing exit.
+# 9. From the remaining terminal window, type history -c. This wipes all history that
+# is currently in memory. Close this terminal session as well.
+# 10. Open a new terminal session and type history. The result may be a bit unex-
+# pected, but you’ll see a list of commands anyway. The reason is that history -c
+# clears the in-memory history, but it does not remove the .bash_history file in
+# your home directory.
+
+# using autocompletion with tab
+# 1. Still from a user shell, type gd and press Tab. You’ll see that nothing happens.
+# 2. Press Tab again. Bash now shows a short list of all commands that start with the
+# letters gd.
+# 3. To make it clear to Bash what you want, type i (so that your prompt at this point
+# shows the command gdi). Press Tab again. Bash now completes the command to
+# gdisk. Press Enter to launch it, and press Enter again to close it.
+# 4. Use cd /etc to go to the /etc directory.
+# 5. Type cat ps and press Tab. Because there is one file only that starts with pas, Bash
+# knows what to do and automatically completes the filename to passwd. Press
+# Enter to execute the command.
+# using tab is important for speed but also correctness for example when tabing in a long path
+
+# vim cheat sheet
+# Command        Explanation
+# Esc            Switches from input mode to command mode. Press this key before typing any command.
+# i, a           Switches from command mode to input mode at (i) or after (a) the current cursor position.
+# o              Opens a new line below the current cursor position and goes to input mode.
+# :wq            Writes the current file and quits.
+# :q!            Quits the file without applying any changes. The ! forces the command to do its work. Add the ! only if you really know what you are doing.
+# :w             filename Writes the current file with a new filename.
+# dd             Deletes the current line and places the contents of the deleted line into memory.
+# yy             Copies the current line.
+# p              Pastes the contents that have been cut or copied into memory.
+# v              Enters visual mode, which allows you to select a block of text using the arrow keys. Use d to cut the selection or y to copy it.
+# u              Undoes the last command. Repeat as often as necessary.
+# Ctrl-r         Redoes the last undo. (Cannot be repeated more than once.)
+# gg             Goes to the first line in the document.
+# G              Goes to the last line in the document.
+# /text          Searches for text from the current cursor position forward.
+# ?text          Searches for text from the current cursor position backward.
+# ^              Goes to the first position in the current line.
+# $              Goes to the last position in the current line.
+# !ls            Adds the output of ls (or any other command) in the current file.
+# :%s/old/new/g  Replaces all occurrences of old with new.
+
+# exercises
+# 1. Type vim ~/testfile. This starts vim and opens a file with the name testfile in ~,
+# which represents your current home directory.
+# 2. Type i to enter input mode and then type the following text:
+# cow
+# sheep
+# ox
+# chicken
+# snake
+# fish
+# oxygen
+# 3. Press Esc to get back to command mode and type :w to write the file using the
+# same filename.
+# 4. Type :3 to go to line number 3.
+# 5. Type dd to delete this line.
+# 6. Type dd again to delete another line.
+# 7. Type u to undo the last deletion.
+# 8. Type o to open a new line.
+# 9. Enter some more text at the current cursor position:
+# tree
+# farm
+# 10. Press Esc to get back into command mode.
+# 11. Type :%s/ox/OX/g and note the changes to the line that contained ox.
+# 12. Type :wq to write the file and quit. If for some reason that does not work,
+# use :wq!
+
+# see all environment variables
+env
+
+# env file locations
+# /etc/profile: This is the generic file that is processed by all users upon login.
+# /etc/bashrc: This file is processed when subshells are started.
+# ~/.bash_profile: In this file, user-specific login shell variables can be defined.
+# ~/.bashrc: In this user-specific file, subshell variables can be defined.
+
+# banners
+# To display messages during the login process, Bash uses the /etc/motd and the /etc/ issue files.
+
+# exercises
+# 1. Type echo $LANG to show the contents of the variable that sets your system
+# keyboard and language settings.
+# 2. Type ls --help. You’ll see that help about the ls command is displayed in the
+# current language settings of your computer.
+# 3. Type LANG=es_ES.UTF-8. This temporarily sets the language variable to
+# Spanish. Type ls --help again to verify.
+# 4. Type exit to close your terminal window. Because you have not changed the
+# contents of any of the previously mentioned files, when you open a new shell, the
+# original value of the LANG variable will be used.
+# 5. Open a shell as user again.
+# 6. Verify the current value of the LANG variable by typing echo $LANG.
+# 7. Type vim .bashrc to open the .bashrc configuration file.
+# 8. In this file, add the line COLOR=red to set a variable with the name COLOR
+# and assign it the value red. Notice that this variable doesn’t really change
+# anything on your system; it just sets a variable.
+# 9. Close the user shell and open a new user shell.
+# 10. Verify that the variable COLOR has been set, by using echo $COLOR.
+# Because the .bashrc file is included in the login procedure, the variable is set after
+# logging in.
+
+# --help
+# Nearly all commands provide a short overview of help when the option --help
+# is used. Some commands do not honor that option and consider it erroneous.
+# Fortunately, these commands will be so friendly as to show an error message,
+# displaying valid options with the command, which effectively means that you’ll get
+# what you needed anyway.
+
+# accessing documentation
+man ls
+# To get to the bottom of the man page as fast as possible, use the G command. 
+# You can also type /example to search the man page for any examples.
+# You can search the mandb database by using apropos or man -k
+
+# update documentation database
+mandb
+
+# exercises
+# 1. Because man -k does not give the expected result, it makes sense to look in the
+# man page for the man command for additional information about man -k. Type
+# man man to open the man page of man. Once in the man page, type /-k to look
+# for a description of the -k option. Type n a few times until you get to the line that
+# describes the option. You’ll see that man -k is equivalent to apropos and that
+# you can read the man page of apropos for more details. So type q to exit this man
+# page.
+# 2. Type man apropos and read the first paragraphs of the description. You’ll see
+# that the database searched by apropos is updated by the mandb program.
+# 3. Type man mandb. This man page explains how to run mandb to update the
+# mandb database. As you’ll read, all you need to do is type mandb, which does the
+# work for you.
+# 4. Type sudo mandb to update the mandb database. Notice that you won’t see
+# many man pages being added if the mandb database was already up to date.
+
+# Using info
+# exercises 
+# 1. Type man ls. Type G to go to the end of the man page and look at the “See
+# Also” section. It tells you that the full documentation for ls is maintained as a
+# Texinfo manual that can be shown with the info command. Quit the man page by
+# pressing q.
+# 2. Type pinfo ‘(coreutils) ls invocation’. This shows the information about ls
+# usage in the pinfo page. Read through it and press q when done. Alternatively,
+# you can use the info command, but pinfo shows nicer formatting.
+
+
 
 # create a new empty file at a location
 touch /tmp/somevimfile
@@ -46,10 +296,6 @@ grep "PATH" * .*
 
 # find : Search for a file or directory with a certain name or type 
 find . -name "*.txt"
-
-# pipe : send the output of one command to another
-find . -name "*.yml" | grep "git" > tst.txt
-history | grep ls
 
 # echo : print something
 # > and >> : redirect output of a command 
